565e2c1fd0f68de17afc5c358fe75a17b7f4080e
sbustos497 <sbustos629@unab.edu.co>
2023-08-26 11:18:27 -0500

    split home

--- src/Pages/Inicio/Home.jsx
+++ src/Pages/Inicio/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Navbar } from '../../Components/Navbar/Navbar'
+import SplitScreen from '../../Components/Split/SplitScreen'
 import './Home.css'
+
 export  function Home() {
   return (
     <div>
         <Navbar />
+        <SplitScreen>
+      <div className='small-column'>
+        Perfil Estudiante
+      </div>
+      <div className='large-column'>
+          Imagen Unab u Horarios
+      </div>
+    </SplitScreen>
+            
+        
     </div>
   )
 }
