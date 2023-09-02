f43b77bced262ebc87a576ef4739eee93f313119
sbustos497 <sbustos629@unab.edu.co>
2023-09-02 09:45:45 -0500

    home

--- src/Pages/Inicio/Home.jsx
+++ src/Pages/Inicio/Home.jsx
@@ -3,20 +3,40 @@ import { Navbar } from '../../Components/Navbar/Navbar'
 import SplitScreen from '../../Components/Split/SplitScreen'
 import './Home.css'
 
-export  function Home() {
+export function Home() {
   return (
     <div>
-        <Navbar />
-        <SplitScreen>
-      <div className='small-column'>
-        Perfil Estudiante
-      </div>
-      <div className='large-column'>
-          Imagen Unab u Horarios
-      </div>
-    </SplitScreen>
+      <Navbar />
+      <SplitScreen>
+        <div className='small-column'>
+          <img src="https://i0.wp.com/flejedecosas.com/wp-content/uploads/perfil-de-usuario-google-chrome-vacio.jpg?ssl=1" alt="Perfil del usuario" id='imgUsuario' />
+          <div className=' datos'>
+            <h5>Nombre del Estudiante</h5>
             
+            <p className='dato'> <b> ID:</b> </p>
+            <p className='dato'> <b> Carrera:</b> </p>
+            <p className='dato'> <b> Tiempo en reunion:</b> </p>
+
+          </div>
+
+
+          <div className="row  ">
+          <button type="button" class="btn btn-light">Horarios</button>
+          <button type="button" class="btn btn-light">Reuniones agendadas</button>
+          <button type="button" class="btn btn-light">Reuniones realizadas</button>
+          <button type="button" class="btn btn-light">Modificar asignatura a impartir</button>
+          </div>
+        </div>
+        <div className='large-column'>
+           <div className='container'>
+           <img src="https://unab.edu.co/wp-content/uploads/2022/01/logo-u-vig.png" alt="UnabImg" id='UnabImg' />
+           
+           </div>
         
+        </div>
+      </SplitScreen>
+
+
     </div>
   )
 }
